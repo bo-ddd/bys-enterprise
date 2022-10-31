b3aa3a3f560358a560093b4ceace4e6b5bd40b33
fuchenzijian <2014873253@qq.com>
2022-10-31 16:45:12 +0800

    update person.vue

---
+++ src/views/findPersonnel/Personnel.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div class="personnel">
+        <div class="wrap filter-wrap"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .personnel{
+        &>h1{
+            background: red;
+        }
+    }
+</style>

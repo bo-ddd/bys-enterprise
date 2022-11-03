f4dcfbf2bece3e32921ae57588a91a9ed324adb3
yang <1145938287@qq.com>
2022-11-03 19:54:50 +0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -192,5 +192,17 @@ export default {
        */
       inviteTalent:()=>{
         return axios.post('/company/inviteTalent',params,postConfig)
-      }
+      },
+      /**
+       * 删除职位
+       */
+      deletePosition:()=>{
+        return axios.post('/company/delPosition',params,postConfig)
+      },
+      /**
+       * 修改职位信息
+       */
+       modifyPosition:()=>{
+        return axios.post('/company/modifyPosition',params,postConfig)
+      },
 }
